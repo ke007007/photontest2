5019fdac6b5bbc68c8895c3e39f879724dbaf55a
MYCOMPUTER\ke007 <ke007007@gmail.com>
2017-06-18 16:19:45 +0900

    Create unity project

---
+++ Assets/PhotonController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonController : MonoBehaviour
+{
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		PhotonNetwork.ConnectUsingSettings ("0.1");
+	}
+
+	void OnJoinedLobby ()
+	{
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnPhotonRandomJoinFailed ()
+	{
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	void OnJoinedRoom ()
+	{
+		PhotonNetwork.Instantiate (
+			player.name,
+			new Vector3 (2f, 1f, -3f),
+			Quaternion.identity,
+			0
+		);
+
+		GameObject mainCamera = GameObject.FindWithTag ("MainCamera");
+//		mainCamera.GetComponent <ThirdPersonCamera> ().enabled = true;
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : Photon.MonoBehaviour
+{
+
+	void FixedUpdate ()
+	{
+		if (!(photonView.isMine)) {
+			return;
+		}
+
+//		float h = Input.GetAxis ("Horizontal");
+//		float v = Input.GetAxis ("Vertical");
+//		anim.SetFloat ("Speed", v);                          
+//		anim.SetFloat ("Direction", h);                      
+//		anim.speed = animSpeed;             
+//		currentBaseState = anim.GetCurrentAnimatorStateInfo (0);
+//		rb.useGravity = true;
+	}
+
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
